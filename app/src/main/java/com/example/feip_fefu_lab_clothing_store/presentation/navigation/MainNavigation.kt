bc62d84ea8d67c63294c92b95c1439c06c5d7239
derushbdh <nikita.zhukov.05@bk.ru>
2026-05-21 23:08:33 +1000

    fix: исправление сохранения состояния навигации через rememberSaveable

--- app/src/main/java/com/example/feip_fefu_lab_clothing_store/presentation/navigation/MainNavigation.kt
+++ app/src/main/java/com/example/feip_fefu_lab_clothing_store/presentation/navigation/MainNavigation.kt
@@ -8,13 +8,14 @@ import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.feip_fefu_lab_clothing_store.presentation.cart.CartScreen
 
 @Composable
 fun MainScreenContainer(content: @Composable (PaddingValues) -> Unit) {
-    var selectedRoute by remember { mutableStateOf("catalog") }
+    var selectedRoute by rememberSaveable { mutableStateOf("catalog") }
 
     Scaffold(
         bottomBar = {
